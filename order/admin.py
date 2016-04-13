8cff6ed50f1b8902529c64332f75190f2bb28b9a
Mikael <mfridh01@gmail.com>
2016-04-13 20:12:31 +0200

    Upload to Git

---
+++ order/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from.models import Kund, Artikel, Order, OrderLista
+
+admin.site.register(Kund)
+admin.site.register(Artikel)
+admin.site.register(Order)
+admin.site.register(OrderLista)
---
+++ order/models.py
@@ -0,0 +1,42 @@
+from __future__ import unicode_literals
+
+from django.db import models
+from decimal import Decimal
+
+class Kund(models.Model):
+    namn = models.CharField(max_length=100)
+    adress_gata = models.CharField(max_length=100)
+    adress_stad = models.CharField(max_length=50)
+    telefonnummer = models.CharField(max_length=15)
+    epost = models.EmailField()
+
+    def __str__(self):
+        return self.namn
+
+class Artikel(models.Model):
+    artikel_namn = models.CharField(max_length=100)
+    typ = models.CharField(max_length=100)
+    antal = models.IntegerField(max_length=None)
+    pris = models.DecimalField(max_digits=8, decimal_places=2, default=Decimal('0.00'))
+
+    def __str__(self):
+        return self.typ + ', ' + self.artikel_namn
+
+
+class Order(models.Model):
+    kund = models.ForeignKey(Kund, on_delete=models.CASCADE)
+    datum_resning = models.DateField()
+    datum_rivning = models.DateField()
+    pristyp = models.CharField(max_length=100, default='Standard')
+
+    def __str__(self):
+        return str(self.datum_resning) + ', ' + str(self.kund)
+
+class OrderLista(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    artikel = models.ForeignKey(Artikel, on_delete=models.CASCADE)
+    antal = models.IntegerField(max_length=None)
+
+
+    def __str__(self):
+        return str(self.order) + ' : ' + str(self.antal) + 'st. ' + str(self.artikel)
