c4c1a2eb7b5734559efddc63e43954639e8bb1f4
mfridh01 <mfridh01@gmail.com>
2016-04-21 22:22:52 +0200

    Fixat ordersidan. Lagt till sökning och snabbknappar.

--- order/views.py
+++ order/views.py
@@ -6,12 +6,22 @@ from .models import Order, OrderLista, Kund, Artikel
 
 def index(request):
 
-    # exempel sokning.
-    sok_datum = "2016-04-09"
-    html = ''
+    # Kolla om man fått en sökning.
+    if 'q' in request.GET:
+        sok_datum = str(request.GET['q'])
+    else:
+        sok_datum = "2016-04-21"
+
+    html = '<form action="/order/" method="get"><input type="text" name="q" value="' + sok_datum + '"><input type="submit" value="Search"></form>'
+    # skriva ut alla resningsdatum som finns.
+    html += '<table><tr><td><h1>Resningar / Hämtningar</h1>'
+    html += alla_resnings_ordrar("resning")
+    html += '</td><td><h1>Rivningar / Återlämningar</h1>'
+    html += alla_resnings_ordrar("rivning")
+    html += '</td></tr></table>'
     pris = 0
 
-    html = '<h1>Sökt datum: ' + sok_datum + '</h1><hr />'
+    html += '<hr /><h1>Sokt datum: ' + sok_datum + '</h1><hr />'
 
     # Hamta alla ordrar och satta variabler.
     alla_ordrar = Order.objects.all().order_by('datum_resning').filter(datum_resning__lte=sok_datum).filter(datum_rivning__gte=sok_datum)
@@ -20,13 +30,25 @@ def index(request):
     dict_antal = alla_artiklar()
 
     # Loopa igenom alla ordrar och hamta orderrad per order.
-    html += '<h3>Aktuella ordrar vid sökt datum.</h3>'
+    html += '<table><tr><td valign="top"><h3>Aktuella ordrar vid sokt datum.</h3>'
     for orderrad in alla_ordrar:
         # Filtrera en orderlista per order.
         hela_orderlistan = OrderLista.objects.filter(order=orderrad)
+        datum_rivning = orderrad.datum_rivning
 
         # Skriva ut vilken order som finns.
-        html += '<u>' + str(orderrad) + '</u><br ><ul>'
+        if str(orderrad.datum_rivning) == sok_datum:
+            str_rivning = "RIVNING / ÅTERLÄMNING"
+            font_color = "#BBFFB7"
+        elif str(orderrad.datum_resning) == sok_datum:
+            str_rivning = "RESNING / HÄMTNING"
+            font_color = "#A5EAFF"
+        else:
+            str_rivning = ""
+            font_color = "white"
+
+        html += '<u style="background-color:' + font_color + ';">' + str(orderrad) + '</u><br >'
+        html += '<b style="background-color:' + font_color + ';">' + str_rivning + '</b><br><ul style="background-color:' + font_color + ';">'
 
 
         # Hamta ut alla artiklar som tillhor ordern.
@@ -40,7 +62,11 @@ def index(request):
             pris += artikel_pris
 
             # Reducera antalet for artikeln.
-            dict_antal[artikel_order_artikel.id] -= artikel_order_antal
+
+            # TODO Testa om det gar att lagga pa om det ar nermontering samma dag.
+            if not str(datum_rivning) == sok_datum:
+                dict_antal[artikel_order_artikel.id] -= artikel_order_antal
+
 
             # Skriva ut artikel
             html += '<li>' + str(artikel_order_antal) + 'st. ' + str(artikel_order_artikel) + ' (' + str(artikel_pris) + 'kr).</li>'
@@ -49,16 +75,17 @@ def index(request):
         pris = 0
 
     # Loopa igenom artikel-listan och hamta ut antalet artiklar kvar.
-    html += '<hr /><h3>Lediga artiklar</h3>'
+    html += '</td><td height="100%" valign="top"><h3>Lediga artiklar</h3>'
     for key, value in dict_antal.items():
         get_artikel = Artikel.objects.get(id=key)
         if value < 0:
-            value_color = "red"
+            value_color = "#FFB5B7"
         else:
-            value_color = "green"
+            value_color = "#BBFFB7"
 
-        html += '<p style="color:' + value_color + '">' + str(get_artikel) + ': ' + str(value) + 'st. kvar.<br />'
+        html += '<li style="background-color:' + value_color + '">' + str(get_artikel) + ': ' + str(value) + 'st. kvar.</li>'
 
+    html += '</td></tr></table>'
     return HttpResponse(html)
 
 # Funktion for att hamta alla artiklar och gora till dictionary.
@@ -69,3 +96,31 @@ def alla_artiklar():
         dict_art[art.id] = art.antal
 
     return dict_art
+
+# Funktion for att hamta alla ordrar och gora till lankar.
+def alla_resnings_ordrar(resning_rivning):
+    html_ordrar = ""
+    senaste_datumet = ""
+    ord_datum = ""
+    counter = 0
+
+    if resning_rivning == "resning":
+        resning_eller_rivning = Order.objects.all().order_by('datum_resning')
+    else:
+        resning_eller_rivning = Order.objects.all().order_by('datum_rivning')
+
+    for ord in resning_eller_rivning:
+        if resning_rivning == "resning":
+            ord_datum = str(ord.datum_resning)
+        else:
+            ord_datum = str(ord.datum_rivning)
+
+        if not ord_datum == senaste_datumet:
+            html_ordrar += '<a href="/order/?q=' + ord_datum + '">' + ord_datum + '</a>, '
+            senaste_datumet = ord_datum
+            counter += 1
+            if counter == 5:
+                html_ordrar += '<br />'
+                counter = 0
+
+    return html_ordrar
